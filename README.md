860a43b0a45692a1c576dc04b0ebd2f2c188be8b
Eman-Abdullah <113295982+Eman-Abdullah@users.noreply.github.com>
2022-12-25 23:29:05 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# HotelReservation
+
+## Eman Alghufaili
+#### I’m now in the United Kingdom living in Guildford, the State is Surrey.
+#### Video Demo:  https://youtu.be/5Su37WwCm8o
+#### Description:
+    This is a basic Hotel Reservation System design using OOP. This project is flexible for as many room as possible. And it support 3 types of rooms that are Standard, Deluxe, and Executive.
+
+    1. In this project we can add as many customer as possible, it will save them. And assign each customer with a unique ID that is generated using there first letter of first name, last name, and last 4 letter of phone number.
+    2. Each room can be book once. For this each room have its own calender that can be set accordingly for almost 3 to 4 months ahead. The purpose of this is also to ensure that the room doesn't appear if another customer want to reserve a room at the same date.
+    3. Reservation can be also modified and cancelled by the customer if they wanna change.
+    4. After reservation, modification, and cancellation a bill is generated that is updated with each task that is mention above.
+    5. The program also show the available rooms during reservation and modification along with availability dates.
+#### Constraints:
+    The program is design only for one hotel and for 3 types of rooms only. Besides that it is only for Reserving rooms not a complete management system that have managing employee capability.
---
+++ project.py
@@ -0,0 +1,134 @@
+from datetime import date
+from src.reservation import Reservation
+from src.reservation import Reservation
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+dateToday = date.today()
+
+
+def main():
+    dRooms = int(input("How many deluxe rooms are in the hotel : "))
+    eRooms = int(input("How many Executive rooms are in the hotel : "))
+    sRooms = int(input("How many Suite rooms are in the hotel : "))
+    availableDays = int(input("For how many days the hotel will be opened : "))
+    reservation = Reservation(sRooms, dRooms, eRooms, availableDays)
+    try:
+        services = int(input("What do you wanna do choose one the following number \n1: Add Customer\n2: Make Reservation\n3: Modify Reservation\n4: Cancel Reservation\n5: Get Reservation Details\n6: To Exit.\nEnter one of the following number:-"))
+    except ValueError:
+        print("Please Enter number from the menu")
+        services = int(input("What do you wanna do choose one the following number \n1: Add Customer\n2: Make Reservation\n3: Modify Reservation\n4: Cancel Reservation\n5: Get Reservation Details\n6: To Exit.\nEnter one of the following number:-"))
+    while(services != 6):
+        if (services == 1):
+            fname = input("Enter First Name : ")
+            lname = input("Enter last name : ")
+            no_of_guest = int(input("Enter number of guests : "))
+            phone_number = int(input("Enter Phone Number of Customer : "))
+            reservation.addCustomer(fname=fname, lname=lname, numGuests=no_of_guest, number=phone_number)
+        elif (services == 2):
+            customerId = input("Enter customer id : ")
+            checkin = input("Enter Check In Date : ")
+            year,month,day = checkin.split('-')
+            checkinDate = date(int(year), int(month), int(day))
+            checkout = input("Enter Check Out Date : ")
+            year,month,day = checkout.split('-')
+            checkoutDate = date(int(year), int(month), int(day))
+            reservation.makeReservation(ID=customerId, chkInDate=checkinDate, chkOutDate=checkoutDate)
+        elif (services == 3):
+            customerId = input("Enter customer id : ")
+            checkin = input("Enter Check In Date : ")
+            year,month,day = checkin.split('-')
+            checkinDate = date(int(year), int(month), int(day))
+            checkout = input("Enter Check Out Date : ")
+            year,month,day = checkout.split('-')
+            checkoutDate = date(int(year), int(month), int(day))
+            reservation.modifyReservation(ID=customerId, newInDate=checkinDate, newOutDate= checkoutDate)
+        elif (services == 4):
+            customerId = input("Enter customer id : ")
+            reservation.cancelReservation(ID=customerId)
+        elif (services == 5):
+            customerId = input("Enter customer id : ")
+            reservation.returnReservationDetails(ID=customerId)
+        elif (services == 6):
+            print("Thank you for using Hotel Reservation System")
+            break
+        else:
+            print("Please Enter correct input")
+        try:
+            services = int(input("What do you wanna do choose one the following number \n1: Add Customer\n2: Make Reservation\n3: Modify Reservation\n4: Cancel Reservation\n5: Get Reservation Details\n6: To Exit.\nEnter one of the following number:-"))
+        except ValueError:
+            print("Please Enter number from the menu")
+            services = int(input("What do you wanna do choose one the following number \n1: Add Customer\n2: Make Reservation\n3: Modify Reservation\n4: Cancel Reservation\n5: Get Reservation Details\n6: To Exit.\nEnter one of the following number:-"))
+        
+main()
+
+def test_Case3(capsys):
+    
+    
+
+    dateVin = dateToday + relativedelta(days = 1)
+    dateVout = dateToday + relativedelta(days = 7)
+    dateVout2 = dateToday + relativedelta(days = 11)
+    dateJin = dateToday + relativedelta(days = 5)
+    dateJout = dateToday + relativedelta(days = 13)
+    
+    reservation = Reservation(1, 1, 1, 30)
+    reservation.addCustomer('Emaan', 'Abdullah', 0, 123456789)
+    reservation.addCustomer('John', 'Doe', 0, 123456789)
+    reservation.makeReservation('EA6789', dateVin, dateVout, "1S")
+    reservation.makeReservation('EA6789', dateVin, dateVout2)
+    reservation.makeReservation('JD6789', dateJin, dateJout, "1E")
+    output = ''
+    with open("testCase3.txt", 'r') as f:
+        for i in f.readlines():
+            output+=i
+    captured = capsys.readouterr()
+    assert captured.out == output
+    
+
+def test_Case4(capsys):
+    
+   
+    
+    dateVIn = dateToday
+    dateOut = dateToday + relativedelta(days = 3)
+    dateOut2 = dateToday + relativedelta(days = 6)
+    dateOut3 = dateToday + relativedelta(days = 4)
+    
+    reservation = Reservation(0, 1, 0, 6)
+    reservation.addCustomer('Emaan', 'Abdullah', 0, 123456789)
+    reservation.addCustomer('John', 'Doe', 0, 123456789)
+    reservation.makeReservation('EA6789', dateVIn, dateOut, '1D')
+    reservation.makeReservation('JD6789', dateOut, dateOut2, '1D')
+    reservation.modifyReservation('EA6789', dateVIn, dateOut3, '1D')
+    output = ''
+    with open("testCase4.txt", 'r') as f:
+        for i in f.readlines():
+            output+=i
+    captured = capsys.readouterr()
+    assert captured.out == output
+def test_Case5(capsys):
+    
+    
+    date1 = dateToday
+    date2 = dateToday + relativedelta(days = 3)
+    date3 = dateToday + relativedelta(days = 6)
+    date4 = dateToday + relativedelta(days = 9)
+    
+    reservation = Reservation(10, 10, 10, 45)
+    reservation.addCustomer('Emaan', 'Abdullah', 0, 123456789)
+    reservation.addCustomer('David', 'Venuto', 0, 123456789)
+    reservation.makeReservation('EA6789', date1, date2, '1D')
+    reservation.makeReservation('DV6789', date3, date4, '2D')
+    
+    reservation.modifyReservation('EA6789', date1, date4, '10E')
+    output = ''
+    with open("testCase5.txt", 'r') as f:
+        for i in f.readlines():
+            output+=i
+    captured = capsys.readouterr()
+    assert captured.out == output
+    
+    
+    
